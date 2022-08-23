c8e0afbb63911bb01ca3ff26f5847284cf8fc6db
rcluff00 <ryan.s.cluff@gmail.com>
2022-08-22 22:52:30 -0600

    everything but fetches

--- public/script.js
+++ public/script.js
@@ -1,4 +1,38 @@
 // TODO: Wire up the app's behavior here.
 // NOTE: The TODOs are listed in index.html
 
-document.queryselector('button').setAttribute('disabled', '');
+document.querySelector('button').setAttribute('disabled', '');
+
+let uvuIdInputDiv = document.getElementsByClassName('uvu-id')[0];
+let uvuIdInput = document.getElementById('uvuId');
+let courseDropdown = document.getElementById('course');
+
+courseDropdown.onchange = function () {
+  if (courseDropdown.selectedIndex != 0) {
+    uvuIdInputDiv.style.display = 'block';
+  } else {
+    uvuIdInputDiv.style.display = 'none';
+  }
+};
+
+uvuIdInput.addEventListener('input', function () {
+  input = uvuIdInput.value;
+  let regex = /^[0-9]{0,10}$/gm;
+  if (!regex.test(input)) {
+    input = input.substring(0, input.length - 1);
+    uvuIdInput.value = input;
+  }
+});
+
+let logs = document.querySelectorAll('ul li');
+for (let i = 0; i < logs.length; i++) {
+  logs[i].addEventListener('click', function () {
+    showHideLog(this);
+  });
+}
+
+function showHideLog(log) {
+  logText = log.querySelector('pre');
+  if (logText.style.display != 'none') logText.style.display = 'none';
+  else logText.style.display = 'block';
+}
